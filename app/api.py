08da2d51d12eea0e12fe551860ce6af80e8931df
Marc-Alexandre Paquet <marcpaquet2@hotmail.com>
2020-09-09 08:20:32 -0400

    accept png, new model and /burntbase endpoint

--- app/api.py
+++ app/api.py
@@ -78,7 +78,7 @@ class UPLOAD(Resource):
                     encoded_string = content["encoded_string"]
 
                     image_bytes = base64.b64decode(str(encoded_string))
-                    img_np = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+                    img_np = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)[:,:,:3]
 
                     detections = yolov5.predict(img_np, max_objects=max_per_class_dict)
 
@@ -91,4 +91,155 @@ class UPLOAD(Resource):
 
         else:
             return make_response(jsonify({"message": "Unauthorized"}), 401)
+
+class BURNTBASE(Resource):
+    def post(self):
+        auth_key = request.headers.get('clash-api-key')
+
+        if auth_key == 'xxxxxxxxxxddddddddddtttttttttt12345':
+            content = request.get_json()
+            if "encoded_string" in content.keys() and len(content.keys()) == 1:
+                #try:
+                encoded_string = content["encoded_string"]
+
+                image_bytes = base64.b64decode(str(encoded_string))
+                img_np = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)[:,:,:3]
+                cv2.imwrite(f'F:/yolov5/data/burntbase/{time.time()}.jpg', img_np)
+
+                print(img_np.shape)
+
+                detections = yolov5.predict(img_np, max_objects=max_per_class_dict)
+
+                height, width, _ = img_np.shape
+
+                resp = img_objects_to_formated_json(detections, width, height)
+
+                return make_response(jsonify(resp), 200)
+                #except Exception as e:
+                    #print(e)
+                    #return make_response(jsonify({"message": "ERROR: Can't process data"}), 422)
+            else:
+                return make_response(jsonify({"message": "ERROR: Invalid POST request"}), 400)
+
+        else:
+            return make_response(jsonify({"message": "Unauthorized"}), 401)
+
+
+def img_objects_to_formated_json(img_objects, img_width, img_height):
+  
+    xmin = 9999
+    xmax = 0
+    ymin = 9999
+    ymax = 0
+    
+    objects = []
+
+    th_level = None
+    th_level_dict = {'eagle':0, 'inferno':0,
+                'air_def':0, 'scatter':0, 'th':0,
+                'wiz_tower': 0, 'xbow': 0, 'champ': 0,
+                'warden': 0}
+
+    for obj in img_objects:
+        if obj['name'] in th_level_dict:
+            th_level_dict[obj['name']] += 1
+
+            if xmin > (obj['bndbox']['xmin'] + obj['bndbox']['xmax'])/2:
+                xmin = (obj['bndbox']['xmin'] + obj['bndbox']['xmax'])/2
+            if xmax < (obj['bndbox']['xmin'] + obj['bndbox']['xmax'])/2:
+                xmax = (obj['bndbox']['xmin'] + obj['bndbox']['xmax'])/2
+            if ymin > (obj['bndbox']['ymin'] + obj['bndbox']['ymax'])/2:
+                ymin = (obj['bndbox']['ymin'] + obj['bndbox']['ymax'])/2
+            if ymax < (obj['bndbox']['ymin'] + obj['bndbox']['ymax'])/2:
+                ymax = (obj['bndbox']['ymin'] + obj['bndbox']['ymax'])/2
+            
+
+    if th_level_dict['scatter'] >= 1 or th_level_dict['champ'] == 1:
+        th_level = 13
+    elif th_level_dict['inferno'] >= 3:
+        th_level = 12
+    elif th_level_dict['eagle'] >= 1 or th_level_dict['warden'] >= 1:
+        th_level = 11
+
+    if th_level == None:
+        print('Not able to infer th level of file ')
+        print(th_level_dict)
+
+    xcenter = (xmin + xmax) / 2
+    ycenter = (ymin + ymax) / 2
+    base_width = xmax - xmin
+    base_height = ymax - ymin
+
+    rel_base_width = base_width/img_width
+    rel_base_height = base_height/img_height
+    rel_starting_x = xmin/img_width
+    rel_starting_y = ymin/img_height
+
+    pos_dict = {}
+    pos_dict['air_def'] = {}
+    pos_dict['air_def']['positions'] = []
+    pos_dict['eagle'] = {}
+    pos_dict['eagle']['positions'] = []
+    pos_dict['inferno'] = {}
+    pos_dict['inferno']['positions'] = []
+    pos_dict['scatter'] = {}
+    pos_dict['scatter']['positions'] = []
+    pos_dict['th'] = {}
+    pos_dict['th']['positions'] = []
+    pos_dict['wiz_tower'] = {}
+    pos_dict['wiz_tower']['positions'] = []
+    pos_dict['xbow'] = {}
+    pos_dict['xbow']['positions'] = []
+
+    for obj in img_objects:
+        if obj['name'] in pos_dict:
+            pos_dict[obj['name']]['positions'].append({
+                "xPercent":round(((obj['bndbox']['xmin'] + obj['bndbox']['xmax'])/2-xmin)/base_width, 14),
+                "yPercent":round(((obj['bndbox']['ymin'] + obj['bndbox']['ymax'])/2-ymin)/base_height, 14)
+            })
+
+    json_dict = {
+        'coord': {
+            'ad':{
+                "positions": pos_dict['air_def']['positions'],
+                "buildingName": "Air Defense"
+            },
+            'ea':{
+                "positions": pos_dict['eagle']['positions'],
+                "buildingName": "Eagle Artillery"
+            },
+            'it':{
+                "positions": pos_dict['inferno']['positions'],
+                "buildingName": "Inferno Tower"
+            },
+            'ss':{
+                "positions": pos_dict['scatter']['positions'],
+                "buildingName": "Scattershot"
+            },
+            'th':{
+                "positions": pos_dict['th']['positions'],
+                "buildingName": "Townhall"
+            },
+            'wt':{
+                "positions": pos_dict['wiz_tower']['positions'],
+                "buildingName": "Wizard Tower"
+            },
+            'xb':{
+                "positions": pos_dict['xbow']['positions'],
+                "buildingName": "X-Bow"
+            }
+        },
+        'matrix': {
+            'size':{
+                'width':rel_base_width,
+                'height':rel_base_height
+            },
+            'startingPosition':{
+                'x':rel_starting_x,
+                'y':rel_starting_y
+            }
+        }
+    }
+
+    return json_dict
         
--- testapi.py
+++ testapi.py
@@ -1,22 +1,23 @@
 import os 
 import requests
-import time
 import base64
 
-API = 'http://localhost:5001'
+API = f'https://llw6mvw5rh.execute-api.ca-central-1.amazonaws.com'
 
-with open(r'data/new/3kMvc2p69AE%00%00%05.jpg', 'rb') as reader:
+# load an image from where ever
+
+image_path = r'data/copied_data/images/3kMvc2p69AE%00%00%05.jpg'
+image_path = 'C:/Users/marcp/Downloads/videocapture_20200904_215946_995283_0_0.jpg'
+with open(image_path, 'rb') as reader:
     image = reader.read()
 
+#convert to base64
 img64_bytes = base64.b64encode(image)
 img64_string = img64_bytes.decode('utf-8')
 post_data = {'encoded_string': img64_string}
 
-print(img64_string)
+#required headers
+headers = {'clash-api-key': 'xxxxxxxxxxddddddddddtttttttttt12345'}
 
-raise
-
-timeit = time.time()
-valid_response = requests.get(f'{API}/upload', json=post_data)
-print(valid_response.text)
-print(time.time()-timeit)
+response = requests.post(f'{API}/burntbase', json=post_data, headers=headers)
+print(response.text)
